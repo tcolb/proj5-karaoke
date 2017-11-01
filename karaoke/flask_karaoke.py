805cb4e14b9c137b5d8157fbe9c41ab0b4ae080e
Tristan Colby <tcolby@Tristans-MacBook-Pro.local>
2017-11-01 11:37:03 -0700

    WAY OVERDUE commit

--- karaoke/flask_karaoke.py
+++ karaoke/flask_karaoke.py
@@ -1,2 +1,30 @@
 import flask
-import logging
+import config
+
+app = flask.Flask(__name__)
+
+CONFIG = config.configuration()
+app.secret_key = CONFIG.SECRET_KEY
+
+FILE_NAME = CONFIG.FILE_NAME
+MAPBOX_TOKEN = CONFIG.MAPBOX_TOKEN
+MAPQUEST_KEY = CONFIG.MAPQUEST_KEY
+
+@app.route("/")
+def index():
+    return flask.render_template('karaoke.html')
+
+@app.route("/_request", methods=["GET"])
+def points():
+
+    points = []
+    with open(FILE_NAME) as f:
+        for line in f:
+            print(line)
+            points.append(line.split(','))
+
+    return flask.jsonify(results=points, box_token=MAPBOX_TOKEN, quest_key=MAPQUEST_KEY)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(port=8080, host="0.0.0.0")
